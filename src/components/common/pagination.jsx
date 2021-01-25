455d2d37b2bc0c46d7608e3567f9948026d86e30
vivekhere <vivekjai10@gmail.com>
2021-01-25 17:36:39 +0530

    Pagination - Handling Page Changes

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import _ from "lodash";
 
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
 
   const pagesCount = Math.ceil(itemsCount / pageSize);
   if (pagesCount === 1) return null;
@@ -12,10 +13,13 @@ const Pagination = (props) => {
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a href="." className="page-link">
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <span className="page-link" onClick={() => onPageChange(page)}>
               {page}
-            </a>
+            </span>
           </li>
         ))}
       </ul>
