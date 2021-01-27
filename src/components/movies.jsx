4b8b5fec69a6cbdfed1661def6e51300171a6e81
vivekhere <vivekjai10@gmail.com>
2021-01-27 18:18:22 +0530

    Sorting - Raising the Sort Event

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,7 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
 
     this.setState({ movies: getMovies(), genres });
   }
@@ -39,6 +39,10 @@ class Movies extends Component {
 
   handleGenreSelect = (genre) => {
     this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
+  handleSort = (path) => {
+    console.log(path);
   };
 
   render() {
@@ -74,6 +78,7 @@ class Movies extends Component {
             movies={movies}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,17 +1,24 @@
+// Raising the sort event
+
+// we will handle the onCLick event for each of the table headings and raise the
+// onSort event and in the movies component we need to handle this onSort event
+// onSort is the function reference or a reference to an event handler in the
+// movies component
+
 import React from "react";
 import Like from "./common/like";
 
 const MoviesTable = (props) => {
-  const { movies, onDelete, onLike } = props;
+  const { movies, onDelete, onLike, onSort } = props;
 
   return (
     <table className="table">
       <thead>
         <tr>
-          <th>Title</th>
-          <th>Stock</th>
-          <th>Genre</th>
-          <th>Rate</th>
+          <th onClick={() => onSort("title")}>Title</th>
+          <th onClick={() => onSort("genre.name")}>Genre</th>
+          <th onClick={() => onSort("numberInStock")}>Stock</th>
+          <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
           <th></th>
           <th></th>
         </tr>
