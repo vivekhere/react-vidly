1b4d5154a5c952e6144fd67df4e36a0b1feec32b
vivekhere <vivekjai10@gmail.com>
2021-01-28 18:27:16 +0530

    Sorting - Extracting TableHeader

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,35 +1,33 @@
 import React, { Component } from "react";
+import TableHeader from "./common/tableHeader";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
-  raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-
-    this.props.onSort(sortColumn);
-  };
+  // This column property does not have to be part of the state because it is
+  // not going to change throughout the lifecycle of the component
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    { key: "like" },
+    { key: "delete" },
+  ];
+  // since path property is used as a unique key and like and delete do not have
+  // path property this will generate an error
+  // So we will pass a key property that can be used as a unique key in absence
+  // of path property
 
   render() {
-    const { movies, onDelete, onLike } = this.props;
+    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
 
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort("title")}>Title</th>
-            <th onClick={() => this.raiseSort("genre.name")}>Genre</th>
-            <th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
-            <th onClick={() => this.raiseSort("dailyRentalRate")}>Rate</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
@@ -59,13 +57,3 @@ class MoviesTable extends Component {
 }
 
 export default MoviesTable;
-
-// Previously we put the logic for determining the sort order in the handleSort
-// event hnadler in the movies component.
-// If in future we want to use the MoviesTable component in other page we will have
-// to re write the logic for sorting on that page.
-// Technically the sorting logic belongs to the MoviesTable component
-// This will prevent from repeating the sorting logic for every page and in the
-// handleSort event handler we will pass the sortColumn instead of the path.
-// We will raise an event raiseSort locally in the component and the put the
-// sorting logic here.
