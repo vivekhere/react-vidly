cd36860aeb9256aa0812598c301f20fae61f3e9e
vivekhere <vivekjai10@gmail.com>
2021-02-04 17:41:10 +0530

    Forms - Refs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef();
+  // this will create a Ref object
+  // in the input element set the Ref attribute to this.username
+
+  // just an example to use the ref object username
+  // componentDidMount() {
+  //   this.username.current.focus();
+  //   // this will focus the username input field.
+  //   // But there is a better method. We could use the autofocus attribute
+  //   // in the input field.
+  // }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    // prevent submission of form to the server
 
-    // call the server -> save the changes -> redirect the user to different page.
-    // For just log 'Submitted'
+    // const username = document.getElementById("username").value;
+    // Now, instead of working with document we will work with -
+    const username = this.username.current.value;
+    // this Ref a property 'current' which returns the actual DOM element
+
     console.log("Submitted");
   };
 
@@ -17,7 +31,13 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
@@ -32,14 +52,16 @@ class LoginForm extends Component {
 
 export default LoginForm;
 
-// If you click on the login button, you will see that the page goes for a full
-// reload. This is not the kind of behaviour we want see, right ?
+// When submitting the form we need the value of the input fields.
+// In plain or vanilla js we can get the value of the input field using
+// document.getELementById('username) - returns an input field
+// tp get the value of the input field add .value at the end.
+// But this is not how we should access DOM elements in React.
 
-// To fix this
-// Every form element has a event called onSubmit.
-// We will raise this event and handle it.
-// We will set this to a handler (handleSubmit) and set it to a function that
-// takes an event object (e) as an argument.
-// This event has an object preventDefault(), this prevents the default
-// behaviour of this event. In this case submiting the form to a server which
-// causes full page reload.
+// In React applications we should never work with the document object because
+// the whole point of React is to put an abstraction over DOM so that we don't
+// work with that directly and with this our applications will be easier to
+// maintain and easier to unit test.
+
+// Inorder to access the username element we need to give it a reference (Refs).
+// But when building a form there is a better way which we will discuss next.
