4095dba53773122a1c11b97d73bc93ba67060286
vivekhere <vivekjai10@gmail.com>
2021-02-14 20:14:43 +0530

    populating the form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import { getGenres } from "../services/fakeGenreService";
-import { getMovie } from "../services/fakeMovieService";
-import { saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/genreService";
+import { getMovie, saveMovie } from "../services/movieService";
 
 class MovieForm extends Form {
   state = {
@@ -34,17 +33,20 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  componentDidMount() {
-    const genres = getGenres(); // fake genre service
+  async componentDidMount() {
+    const { data: genres } = await getGenres(); // fake genre service
     this.setState({ genres });
 
     const movieId = this.props.match.params.id; // get movie id from url
     if (movieId === "new") return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) });
+    try {
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
   }
 
   mapToViewModel(movie) {
